2b9659fc1eec89353183555ae4ebe97e9123fe16
Brandon Annin <brandonannin@gmail.com>
2017-06-28 22:19:29 +0700

    debugging travis

--- cibuild.sh
+++ cibuild.sh
@@ -27,14 +27,25 @@ cd _site
 git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
 cd ..
 
+echo "cat ./vieques.html"
+cat ./vieques.html
+
 # Clean out existing contents
 rm -rf _site/**/* || exit 0
+
+echo "ls -la _site"
+ls -la _site
 
 # Run our compile script
 doCompile
 
 # Now let's go have some fun with the cloned repo
 cd _site
+echo "ls -la _site"
+ls -la
+echo "cat ./vieques.html"
+cat ./vieques.html
+
 git config user.name "Travis CI allover.earth"
 git config user.email "travis.build@allover.earth"
 

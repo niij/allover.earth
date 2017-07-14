6e5ca76e98d4a2816c31f67aee355b1f3a9b5326
Brandon Annin <brandonannin@gmail.com>
2017-07-14 19:33:59 +0900

    revert cibuild.sh back to 933e083029b3a8d169f625aa549e31f6b3ad1542

--- cibuild.sh
+++ cibuild.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
 set -e # Exit with nonzero exit code if anything fails
 
-BUILD_DIR="_site"
 SOURCE_BRANCH="master"
 TARGET_BRANCH="gh-pages"
-
-# Get the deploy key by using Travis's stored variables to decrypt travis_deploy.enc
-ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
-ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
-ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
-ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in travis_deploy.enc -out travis_deploy -d
-chmod 600 travis_deploy
-eval `ssh-agent -s`
-ssh-add travis_deploy
 
 function doCompile {
   ./compile.sh
@@ -31,46 +20,46 @@ REPO=`git config remote.origin.url`
 SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
 SHA=`git rev-parse --verify HEAD`
 
-rm -rf $BUILD_DIR
-git clone $SSH_REPO $BUILD_DIR
-cd $BUILD_DIR
-git checkout $TARGET_BRANCH || exit 20
-git rm -rf .
+# Clone the existing gh-pages for this repo into _site/
+# Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deploy)
+git clone $REPO _site
+cd _site
+git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
 cd ..
 
-#######################
-echo "la _site after rm -rf"
-ls -la _site
+# Clean out existing contents
+rm -rf out/**/* || exit 0
 
 # Run our compile script
 doCompile
 
-############
-echo "la _site after doCompile"
-ls -la _site
-
 # Now let's go have some fun with the cloned repo
-cd $BUILD_DIR
-
-##############
-echo "git status inside _site"
-git status
-
+cd _site
 git config user.name "Travis CI allover.earth"
 git config user.email "travis.build@allover.earth"
 
-# If there are no changes to the compiled $BUILD_DIR (e.g. this is a README update) then just bail.
+# If there are no changes to the compiled _site (e.g. this is a README update) then just bail.
 if git diff --quiet; then
-    echo "No changes to the output on this push"
+    echo "No changes to the output on this push; exiting."
+else
+  echo "changes"
+  git diff
 fi
-
-###################
-echo "la img/"
-ls -la img/
 
 # Commit the "changes", i.e. the new version.
 # The delta will show diffs between new and old versions.
-git commit -am "Deploy to GitHub Pages: ${SHA}"
+git add -A .
+git commit -m "Deploy to GitHub Pages: ${SHA}"
+
+# Get the deploy key by using Travis's stored variables to decrypt travis_deploy.enc
+ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../travis_deploy.enc -out ../travis_deploy -d
+chmod 600 ../travis_deploy
+eval `ssh-agent -s`
+ssh-add ../travis_deploy
 
 # Now that we're all set up, we can push.
 git push $SSH_REPO $TARGET_BRANCH

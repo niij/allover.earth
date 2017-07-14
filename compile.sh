74c0ac9b320536bb91dfe92abf6d4a67397854f6
Brandon Annin <brandonannin@gmail.com>
2017-07-14 15:34:46 +0900

    Adding Grunt to the project to progressiveJPEG/compress all images during CI using imagemin.

--- compile.sh
+++ compile.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e # Exit with nonzero exit code if anything fails
 
-bundle install
-bundle exec jekyll build
+bundle install # ruby stuff for Jekyll
+npm install # Get grunt and image processing deps installed
+grunt build # run imagemin (progressive JPEG and image compression), then jekyll build

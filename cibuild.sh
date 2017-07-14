ae8fedfb7587bca82de398e25fc007907d0b5a38
Brandon Annin <brandonannin@gmail.com>
2017-07-14 19:17:50 +0900

    pull the repo using git instead of HTTPS

--- cibuild.sh
+++ cibuild.sh
@@ -5,6 +5,16 @@ BUILD_DIR="_site"
 SOURCE_BRANCH="master"
 TARGET_BRANCH="gh-pages"
 
+# Get the deploy key by using Travis's stored variables to decrypt travis_deploy.enc
+ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../travis_deploy.enc -out ../travis_deploy -d
+chmod 600 ../travis_deploy
+eval `ssh-agent -s`
+ssh-add ../travis_deploy
+
 function doCompile {
   ./compile.sh
 }
@@ -22,7 +32,7 @@ SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
 SHA=`git rev-parse --verify HEAD`
 
 rm -rf $BUILD_DIR
-git clone $REPO $BUILD_DIR
+git clone $SSH_REPO $BUILD_DIR
 cd $BUILD_DIR
 git checkout $TARGET_BRANCH || exit 20
 git rm -rf .
@@ -61,16 +71,6 @@ ls -la img/
 # Commit the "changes", i.e. the new version.
 # The delta will show diffs between new and old versions.
 git commit -am "Deploy to GitHub Pages: ${SHA}"
-
-# Get the deploy key by using Travis's stored variables to decrypt travis_deploy.enc
-ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
-ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
-ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
-ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../travis_deploy.enc -out ../travis_deploy -d
-chmod 600 ../travis_deploy
-eval `ssh-agent -s`
-ssh-add ../travis_deploy
 
 # Now that we're all set up, we can push.
 git push $SSH_REPO $TARGET_BRANCH
